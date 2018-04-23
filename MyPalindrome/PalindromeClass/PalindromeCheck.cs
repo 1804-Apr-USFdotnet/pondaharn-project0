4a2dd30c348b71908f4b72934ab32e062211ce19
tonep <tone.pondaharn@protonmail.com>
2018-04-23 11:25:37 -0400

    added coding challenge

---
+++ MyPalindrome/PalindromeClass/PalindromeCheck.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PalindromeClass
+{
+    public class PalindromeCheck
+    {
+        // TODO: abstract class and overload IsPalindrome method
+        public static bool IsPalindrome (string str)
+        {
+            var sb = new StringBuilder();
+
+            // normalize
+            // strip punctuation & rmv capitaliztion
+            foreach (char c in str)
+            {
+                if (!char.IsPunctuation(c))
+                    sb.Append(Char.ToLower(c));
+            }
+
+            // string to array
+            str = sb.ToString();
+            char[] arrayStr = str.ToCharArray();
+
+            // reversed array and back to string
+            Array.Reverse(arrayStr);
+            string backwardsStr = new string(arrayStr);
+
+            // return comparison
+            return str == backwardsStr;
+            
+        }
+        
+    }
+}
---
+++ MyPalindrome/TestPalindromeClass/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PalindromeClass;
+
+namespace TestPalindromeClass
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            PalindromeCheck test = new PalindromeCheck();
+        }
+    }
+}
